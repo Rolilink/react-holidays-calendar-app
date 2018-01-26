06ebf726891bda531f9f7f8ef656e6baa268458f
Rolando Perez <me@rolilink.com>
2018-01-25 21:22:09 -0500

    connected calendar

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import Calendar from '../components/Calendar';
+import { connect } from 'react-redux';
 
-export default class CalendarContainer extends Component {
+const transformHolidaysToDates = (holidays) => {
+  return Object.keys(holidays).map((key) => moment(key, 'YYYY-MM-DD').toDate());
+}
+
+class CalendarContainer extends Component {
   render() {
+    const { startDate, numberOfDays, holidays } = this.props;
+
+    if (!startDate && !numberOfDays) {
+      return null;
+    }
+
     return (
         <div className="row">
           <div className="col-md-8 col-md-offset-2">
-            <Calendar startDate={moment('2018-1-24').toDate()} endDate={moment('2018-2-28').toDate()} holidays={[moment('2018-2-1').toDate(), moment('2018-1-21').toDate()]}/>
+            <Calendar
+            startDate={moment(startDate, 'YYYY/MM/DD').toDate()}
+            endDate={moment(startDate, 'YYYY/MM/DD').add(numberOfDays, 'days').toDate()}
+            holidays={transformHolidaysToDates(holidays)}/>
           </div>
         </div>
     );
   }
 }
+
+export const component = CalendarContainer;
+
+const mapStateToProps = (state) => {
+  const { startDate, numberOfDays, holidays } = state;
+
+  return {
+    startDate,
+    numberOfDays,
+    holidays: holidays,
+  }
+}
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CalendarContainer);
